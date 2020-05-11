321fa54be2d88f9727e7f53984e5a8a62a82bf63
akjha96 <akjha96@gmail.com>
2020-05-11 15:37:35 +0530

    Updates

---
+++ precache-manifest.6a38fc1e398b702635cfecd7a9574ec1.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dee6653832a26bfd6dbab1c52350640",
+    "url": "/ReactifolioBs4_Akj/index.html"
+  },
+  {
+    "revision": "c09c738d1d4e66ef6101",
+    "url": "/ReactifolioBs4_Akj/static/css/main.7e47a5fb.chunk.css"
+  },
+  {
+    "revision": "a9d9bd12e6147ed3eb2c",
+    "url": "/ReactifolioBs4_Akj/static/js/2.59cd43b2.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/ReactifolioBs4_Akj/static/js/2.59cd43b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c09c738d1d4e66ef6101",
+    "url": "/ReactifolioBs4_Akj/static/js/main.509674e1.chunk.js"
+  },
+  {
+    "revision": "1c6ccc0b1b6783e86a0f",
+    "url": "/ReactifolioBs4_Akj/static/js/runtime-main.91648183.js"
+  },
+  {
+    "revision": "9730fefc26bb187c4f4bb29b95d82e7a",
+    "url": "/ReactifolioBs4_Akj/static/media/Robofriends.9730fefc.webp"
+  },
+  {
+    "revision": "21999f09aca6272cd15a16d949434923",
+    "url": "/ReactifolioBs4_Akj/static/media/accenture-3.21999f09.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/ReactifolioBs4_Akj/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/ReactifolioBs4_Akj/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/ReactifolioBs4_Akj/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "db3c445b93fa7336d63f617a5f087c7e",
+    "url": "/ReactifolioBs4_Akj/static/media/brain.db3c445b.webp"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/ReactifolioBs4_Akj/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/ReactifolioBs4_Akj/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/ReactifolioBs4_Akj/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/ReactifolioBs4_Akj/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/ReactifolioBs4_Akj/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/ReactifolioBs4_Akj/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/ReactifolioBs4_Akj/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/ReactifolioBs4_Akj/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/ReactifolioBs4_Akj/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/ReactifolioBs4_Akj/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "d13a07af1079666fb65d0eb17ae90253",
+    "url": "/ReactifolioBs4_Akj/static/media/logow.d13a07af.webp"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/ReactifolioBs4_Akj/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/ReactifolioBs4_Akj/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "95dd3e00f0920b6bed086d4a30d5b027",
+    "url": "/ReactifolioBs4_Akj/static/media/portfolioUsingDjango.95dd3e00.webp"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/ReactifolioBs4_Akj/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4bef4c3e368bd761b543e597d90b79bc",
+    "url": "/ReactifolioBs4_Akj/static/media/product_hunt_clone.4bef4c3e.webp"
+  },
+  {
+    "revision": "210d83de03dbbcf3c1a8689dbbb2b5b1",
+    "url": "/ReactifolioBs4_Akj/static/media/profile.210d83de.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/ReactifolioBs4_Akj/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "2a5335d52070f6ad653a12df2a36fdd2",
+    "url": "/ReactifolioBs4_Akj/static/media/pythonanywhere.2a5335d5.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/ReactifolioBs4_Akj/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/ReactifolioBs4_Akj/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/ReactifolioBs4_Akj/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/ReactifolioBs4_Akj/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/ReactifolioBs4_Akj/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/ReactifolioBs4_Akj/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/ReactifolioBs4_Akj/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/ReactifolioBs4_Akj/static/media/slide3.8e5b87b6.webp"
+  }
+]);
