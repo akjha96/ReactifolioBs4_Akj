8e5d95b3c803aced954f8ce27081545e9ceeb35c
akjha96 <akjha96@gmail.com>
2020-08-19 23:34:24 +0530

    removed particles

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 import ContactForm from "./pages/contact-form/contact-form.component";
 import { Parallax } from "react-parallax";
-import Particles from "react-particles-js";
-import { particlesOptions } from "./particlesOptions";
+// import Particles from "react-particles-js";
+// import { particlesOptions } from "./particlesOptions";
 import FooterPanel from "./components/footer/footer.component";
 
 import "./App.css";
@@ -23,10 +23,10 @@ const App = () => {
       <MyCarousal />
       <MyTitleMessage />
       <MyNavbar />
-      <Particles
+      {/* <Particles
         className="particles particles-box"
         params={particlesOptions}
-      />
+      /> */}
 
       <div>
         <Parallax
