8a205dd0cbff7680ada542f5bfb2b5f301f85e37
ANAND KUMAR JHA <akjha96@gmail.com>
2020-10-09 16:48:39 +0530

    Updated link for resume

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -39,7 +39,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/11SO2NSSzldx8d4prufvYG34_fMjCKzI4/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1zz5GMI7JLOhs0bu20pAHyGUl___kBtVm/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
