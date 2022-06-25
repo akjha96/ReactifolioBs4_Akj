481b1bded63cecbe7c27b4a43cb845196ff198f4
ANAND KUMAR JHA <akjha96@gmail.com>
2022-06-25 12:46:40 +0530

    Update about.component.jsx

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -55,7 +55,7 @@ const About = () => {
                   </div>
                   <div>
                     <a
-                      href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing"
+                      href="https://drive.google.com/file/d/1zjdRwWFQ0JeiIVXSRXct8ZqZDZkF6qlR/view?usp=sharing"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
